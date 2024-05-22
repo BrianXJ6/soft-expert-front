07ba72daf138ff6b9794715943f19ec894f1e958
Brian Barros <brian.ferreira@pie.tax>
2024-05-21 22:02:56 -0300

    Project settings such as layout, proxy, etc.

---
+++ layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex flex-column h-100">
+    <header>
+      <b-navbar toggleable="md" type="dark" variant="dark">
+        <div class="container">
+          <b-navbar-brand to="/">
+            Soft Expert
+          </b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse" />
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+              <!-- <b-nav-item to="/">Example</b-nav-item> -->
+              <b-nav-item-dropdown text="Tipos de produto" right>
+                <b-dropdown-item to="/tipos/listagem">
+                  Listar
+                </b-dropdown-item>
+                <b-dropdown-item to="/tipos/novo">
+                  Criar
+                </b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item-dropdown text="Produtos" right>
+                <b-dropdown-item to="/produtos/listagem">
+                  Listar
+                </b-dropdown-item>
+                <b-dropdown-item to="/produtos/novo">
+                  Criar
+                </b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+      </b-navbar>
+    </header>
+
+    <main class="flex-shrink-0 py-5">
+      <nuxt class="container" />
+    </main>
+
+    <footer class="mt-auto py-3 small bg-dark text-secondary">
+      <div class="container">
+        <p class="m-0">
+          Soft Expert - Todos os direitos reservados
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    const nuxtDiv = document.getElementById('__nuxt');
+    const layoutDiv = document.getElementById('__layout');
+
+    if (nuxtDiv) {
+      nuxtDiv.classList.add('h-100');
+    }
+
+    if (layoutDiv) {
+      layoutDiv.classList.add('h-100');
+    }
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,13 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    htmlAttrs: {
+      class: 'h-100'
+    },
+    bodyAttrs: {
+      class: 'h-100'
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -45,8 +51,20 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    // baseUrl: 'http://localhost',
+    proxy: true,
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    }
+  },
+
+  proxy: {
+    // Simple proxy
+    '/api/': {
+      target: 'http://localhost',
+      pathRewrite: { '^/api/': '' }
+    }
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
